2901a3ef63442bdea773070d7b2c7d52f0195342
Jay Pitman <jakeseanp@gmail.com>
2021-05-15 12:49:49 +1000

    Add Line type

--- src/components/TestMode/TestMode.tsx
+++ src/components/TestMode/TestMode.tsx
@@ -4,16 +4,16 @@ import chess from 'chess'
 import Board from '../Board'
 import MovesList from '../MovesList'
 import styles from "./TestMode.scss"
-import { Move, MovesListType } from "../../sharedTypes"
+import { MovesListType, Line } from "../../sharedTypes"
 
 const TestMode = () => {
   const gameClient = chess.create()
   const [board, setBoard] = useState(gameClient.game.board)
-  const [line, setLine] = useState<MovesListType>()
+  const [line, setLine] = useState<Line>()
 
   return(
     <div className={styles.container}>
-      <MovesList turns={line}></MovesList>
+      <MovesList turns={line.moves}></MovesList>
     </div>
   )
 }
--- src/sharedTypes/index.ts
+++ src/sharedTypes/index.ts
@@ -2,5 +2,6 @@ import PieceName from "./PieceName"
 import Side from "./Side"
 import Move from "./Move"
 import MovesListType from "./MovesList"
+import Line from "./Line"
 
-export { PieceName, Side, Move, MovesListType }
+export { PieceName, Side, Move, MovesListType, Line }
