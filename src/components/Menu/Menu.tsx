a85597651c4e5e469376b9b1be57d5c70d365d7c
Jay Pitman <jakeseanp@gmail.com>
2021-06-01 22:34:44 +1000

    Use images instead of buttons in Menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,6 +2,7 @@ import React, { Dispatch, SetStateAction } from "react";
 
 import Button from "../Button";
 import styles from "./Menu.scss";
+import { FreeImg, TestImg, ListImg } from "./images";
 
 type Mode = "menu" | "free" | "test" | "list";
 
@@ -12,14 +13,23 @@ type Props = {
 const Menu = ({ setMode }: Props) => {
   return (
     <div className={styles.container}>
-      <div className={styles.buttonContainer}>
-        <Button text="FREE" onClick={() => setMode("free")} />
+      <div
+        className={styles.outerButtonContainer}
+        onClick={() => setMode("free")}
+      >
+        <div className={styles.buttonContainer + " " + styles.free} />
       </div>
-      <div className={styles.buttonContainer}>
-        <Button text="TEST" onClick={() => setMode("test")} />
+      <div
+        className={styles.outerButtonContainer}
+        onClick={() => setMode("test")}
+      >
+        <div className={styles.buttonContainer + " " + styles.test} />
       </div>
-      <div className={styles.buttonContainer}>
-        <Button text="LIST" onClick={() => setMode("list")} />
+      <div
+        className={styles.outerButtonContainer}
+        onClick={() => setMode("list")}
+      >
+        <div className={styles.buttonContainer + " " + styles.list} />
       </div>
     </div>
   );
---
+++ src/components/Menu/images/index.ts
@@ -0,0 +1,3 @@
+export const FreeImg = require("./free.png");
+export const TestImg = require("./test.png");
+export const ListImg = require("./list.png");
