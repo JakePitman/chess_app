5210d80aea44ae34ee11b78cf8784a41ef408a54
Jay Pitman <jakeseanp@gmail.com>
2021-06-01 22:56:20 +1000

    Add tooltips for menu buttons

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -17,18 +17,21 @@ const Menu = ({ setMode }: Props) => {
         className={styles.outerButtonContainer}
         onClick={() => setMode("free")}
       >
+        <div className={styles.tooltip}>Free</div>
         <div className={styles.buttonContainer + " " + styles.free} />
       </div>
       <div
         className={styles.outerButtonContainer}
         onClick={() => setMode("test")}
       >
+        <div className={styles.tooltip}>Test</div>
         <div className={styles.buttonContainer + " " + styles.test} />
       </div>
       <div
         className={styles.outerButtonContainer}
         onClick={() => setMode("list")}
       >
+        <div className={styles.tooltip}>List</div>
         <div className={styles.buttonContainer + " " + styles.list} />
       </div>
     </div>
