ee90eb79dc15346507c9139563659bcd9a9eeabf
Jay Pitman <jakeseanp@gmail.com>
2021-06-05 20:45:58 +1000

    Add ability to change starting point
    TODO: pass maximum possible starting point into CommandColumn from
    TestMode (length of shortest line, minus two)

--- src/components/TestMode/CommandColumn.tsx
+++ src/components/TestMode/CommandColumn.tsx
@@ -3,6 +3,68 @@ import React, { useState, SetStateAction, Dispatch } from "react";
 import styles from "./CommandColumn.scss";
 import Button from "../Button";
 
+const StartingPointMenuOption = ({
+  value,
+  setStartingPoint,
+  closeMenu,
+  isSelected,
+}: {
+  value: number | "random";
+  setStartingPoint: Dispatch<SetStateAction<number | "random">>;
+  closeMenu: () => void;
+  isSelected: boolean;
+}) => {
+  return (
+    <div
+      className={
+        styles.startingPointMenuOption +
+        ` ${isSelected && styles.selectedStartingPointOption}`
+      }
+      onClick={() => {
+        setStartingPoint(value);
+        closeMenu();
+      }}
+    >
+      {value}
+    </div>
+  );
+};
+
+const StartingPointMenu = ({
+  maximumStartingPoint,
+  setStartingPoint,
+  closeMenu,
+  currentStartingPoint,
+}: {
+  maximumStartingPoint: number;
+  setStartingPoint: Dispatch<SetStateAction<number | "random">>;
+  closeMenu: () => void;
+  currentStartingPoint: number | "random";
+}) => {
+  const toMaximumStartingPoint = Array.from(
+    { length: maximumStartingPoint },
+    (_, i) => i + 1
+  );
+  return (
+    <div className={styles.startingPointMenuContainer}>
+      {toMaximumStartingPoint.map((e, i) => (
+        <StartingPointMenuOption
+          value={i + 1}
+          setStartingPoint={setStartingPoint}
+          closeMenu={closeMenu}
+          isSelected={currentStartingPoint === i + 1}
+        />
+      ))}
+      <StartingPointMenuOption
+        value={"random"}
+        setStartingPoint={setStartingPoint}
+        closeMenu={closeMenu}
+        isSelected={currentStartingPoint === "random"}
+      />
+    </div>
+  );
+};
+
 type Props = {
   setRandomLine: () => void;
   lineTitle: string;
@@ -10,14 +72,38 @@ type Props = {
   setStartingPoint: Dispatch<SetStateAction<number | "random">>;
 };
 
-const CommandColumn = ({ setRandomLine, lineTitle }: Props) => {
+const CommandColumn = ({
+  setRandomLine,
+  lineTitle,
+  startingPoint,
+  setStartingPoint,
+}: Props) => {
   const [message, setMessage] = useState<string>("");
+  const [startingPointPopupActive, setStartingPointPopupActive] =
+    useState<boolean>(false);
 
   return (
     <div className={styles.container}>
-      <p className={styles.inputHeader}>{lineTitle}</p>
-      <Button text="New line" onClick={setRandomLine} />
-      <p className={styles.message}>{message}</p>
+      {startingPointPopupActive ? (
+        <StartingPointMenu
+          maximumStartingPoint={40}
+          setStartingPoint={setStartingPoint}
+          closeMenu={() => setStartingPointPopupActive(false)}
+          currentStartingPoint={startingPoint}
+        />
+      ) : (
+        <>
+          <p className={styles.inputHeader}>{lineTitle}</p>
+          <Button text="New line" onClick={setRandomLine} />
+          <Button
+            text={`Starting point: ${startingPoint}`}
+            onClick={() => {
+              setStartingPointPopupActive(true);
+            }}
+          />
+          <p className={styles.message}>{message}</p>
+        </>
+      )}
     </div>
   );
 };
