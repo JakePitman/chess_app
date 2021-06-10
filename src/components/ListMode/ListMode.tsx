7b51e8a90437e2e43b027b5c23c1555939e0d430
Jay Pitman <jakeseanp@gmail.com>
2021-06-10 17:44:32 +1000

    Add & style variation to list

--- src/components/ListMode/ListMode.tsx
+++ src/components/ListMode/ListMode.tsx
@@ -68,22 +68,20 @@ const handleDELETE = (id: number, updateLinesFromDB: () => void) => {
 };
 
 type LineRowProps = {
-  title: string;
-  selected: boolean;
-  id: number;
+  line: Line;
   updateLinesFromDB: () => void;
   isWhiteLine: boolean;
   searchInputValue: string;
 };
 
 const LineRow = ({
-  title,
-  selected,
-  id,
+  line,
   updateLinesFromDB,
   isWhiteLine,
   searchInputValue,
 }: LineRowProps) => {
+  const { name, variation, selected, id } = line;
+  const splitSearchValues = searchInputValue.toLowerCase().split(" ");
   return (
     <div
       className={
@@ -92,16 +90,26 @@ const LineRow = ({
     >
       <div
         className={styles.checkbox + ` ${selected && styles.selected}`}
-        onClick={() => handlePUT(title, selected, updateLinesFromDB)}
+        onClick={() => handlePUT(name, selected, updateLinesFromDB)}
       />
-      <Highlighter
-        className={styles.lineTitleContainer}
-        highlightClassName={styles.lineTitleHighlighted}
-        unhighlightClassName={styles.lineTitle}
-        searchWords={searchInputValue.toLowerCase().split(" ")}
-        autoEscape={true}
-        textToHighlight={title}
-      />
+      <div className={styles.lineRowTextContainer}>
+        <Highlighter
+          className={styles.lineTitleContainer}
+          highlightClassName={styles.lineTitleHighlighted}
+          unhighlightClassName={styles.lineTitle}
+          searchWords={splitSearchValues}
+          autoEscape={true}
+          textToHighlight={name}
+        />
+        <Highlighter
+          className={styles.lineVariationContainer}
+          highlightClassName={styles.lineTitleHighlighted}
+          unhighlightClassName={styles.lineVariation}
+          searchWords={splitSearchValues}
+          autoEscape={true}
+          textToHighlight={variation}
+        />
+      </div>
       <p
         className={styles.deleteButton}
         onClick={() => handleDELETE(id, updateLinesFromDB)}
@@ -124,9 +132,7 @@ const renderRows = (
       {lines.map((line) => {
         return line.playercolor === filterColor ? (
           <LineRow
-            title={line.name}
-            selected={line.selected}
-            id={line.id}
+            line={line}
             updateLinesFromDB={updateLinesFromDB}
             isWhiteLine={line.playercolor === "white"}
             searchInputValue={searchInputValue}
